e70197772be776f6eac9c49d74a82aac4a589317
rrconey <roscoe@npmjs.com>
2019-08-17 15:46:52 -0500

    made an update to the title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import cleaner from './cleaner.jpg'
 import './App.css';
+import NavbarPage from './Navi'
 
 function App() {
   return (
     <div className="App">
+      <NavbarPage />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <img src={cleaner} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nando's Cleaning Services
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
